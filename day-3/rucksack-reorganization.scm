2c006f53b7311156320bde1a7b2ef5841019770b
Andrew Patterson <andrewpatt7@gmail.com>
2022-12-03 15:37:34 -0500

    Start of day 3

---
+++ day-3/rucksack-reorganization.scm
@@ -0,0 +1,14 @@
+(define-module (advent-of-code-2022 day-3 rucksack-reorganization)
+  #:use-module (advent-of-code-2022 utils)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match))
+
+(define test-input
+  '("vJrwpWtwJgWrhcsFMMfFFhFp"
+    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+    "PmmdzqPrVvPwwTWBwg"
+    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+    "ttgJtRGJQctTZtZT"
+    "CrZsJsPPZsGzwwsLwLmpwMDw"
+    ""))
+
