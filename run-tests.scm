25579997e93f8008b300e71600d970885a8e6c79
Andrew Patterson <andrewpatt7@gmail.com>
2022-12-06 17:17:08 -0500

    Added test running script
    It uses module reflection!

---
+++ run-tests.scm
@@ -0,0 +1,52 @@
+#!/usr/bin/env -S guile -e '(@ (run-tests) main)' -s
+!#
+
+(add-to-load-path (dirname (current-filename)))
+
+(define-module (run-tests)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 ftw)
+  #:use-module (utils)
+  #:use-module (srfi srfi-1)
+  #:use-module (pipe)
+  #:export (run-all-tests main))
+
+(define path (dirname (current-filename)))
+
+;; Based heavily on `file-system-tree`s implementation
+(define (get-module-names-under dir)
+  (define (enter? name stat result)
+	(not (equal? name ".git")))
+  (define (leaf name stat result)
+	(if (equal? (string-take-right name 4) ".scm")
+		(match result
+		  (((siblings ...) rest ...)
+		   (cons (cons (list (string->symbol
+                              (string-drop-right (basename name) 4)))
+					   siblings)
+				 rest)))
+		result))
+  (define (down name stat result)
+	(cons '() result))
+  (define (up name stat result)
+	(match result
+	  (((children ...) (siblings ...) rest ...)
+	   (cons (append (map (curry cons (string->symbol (basename name)))
+						  children)
+					 siblings)
+			 rest))))
+  (define (skip _name _stat result) result)
+  (car (file-system-fold enter? leaf down up skip error '(()) dir)))
+
+(define (run-all-tests)
+  (->> (get-module-names-under path)
+       (map cdr)
+       (map (lambda (lst)
+              (resolve-module lst #:ensure #f)))
+       (for-each (lambda (module)
+                   (when module
+                     (when-let (var (module-variable module 'tests))
+                       ((variable-ref var))))))))
+
+(define (main . args)
+  (run-all-tests))
