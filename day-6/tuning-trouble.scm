70a8e3869a4eb23f195a5ae1bd50fb6c4eaf9d9a
Andrew Patterson <andrewpatt7@gmail.com>
2022-12-06 15:33:16 -0500

    Day 6 start

---
+++ day-6/tuning-trouble.scm
@@ -0,0 +1,22 @@
+(define-module (day-6 tuning-trouble)
+  #:use-module (test)
+  #:use-module (utils)
+  #:use-module (ice-9 match)
+  #:use-module (srfi srfi-1)
+  #:use-module (pipe))
+
+(define test-inputs
+  '("mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    "nppdvjthqldpwncqszvftbrmjlhg"
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"))
+
+(define (tests)
+  (in-test-suite ("Day 6: Tuning Trouble")
+    (test-equal
+        (map star-1 test-inputs)
+        '(7 5 6 10 11))))
+
+
+
