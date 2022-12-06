a9338b196b28eb436f582b3764f783ad1a02958c
Andrew Patterson <andrewpatt7@gmail.com>
2022-12-06 15:45:37 -0500

    Day 6 Star 1

--- day-6/tuning-trouble.scm
+++ day-6/tuning-trouble.scm
@@ -2,6 +2,7 @@
   #:use-module (test)
   #:use-module (utils)
   #:use-module (ice-9 match)
+  #:use-module (ice-9 textual-ports)
   #:use-module (srfi srfi-1)
   #:use-module (pipe))
 
@@ -20,3 +21,20 @@
 
 
 
+(define (all-unique lst)
+  (= (length lst)
+     (length (delete-duplicates lst equal?))))
+
+(define (start-of-packet-index buffer-list)
+  (let rec ((buffer (drop buffer-list 4))
+            (index 3)
+            (last-4-chars (reverse (take buffer-list 4))))
+    (if (all-unique last-4-chars)
+        index
+        (rec (cdr buffer)
+             (1+ index)
+             (take (cons (car buffer) last-4-chars)
+                   4)))))
+
+(define (star-1 buffer)
+  (1+ (start-of-packet-index (string->list buffer))))
