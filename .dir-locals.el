2cb7857923de27b6039ab71042030fab8261e936
Andrew Patterson <andrewpatt7@gmail.com>
2022-12-06 19:08:09 -0500

    .dir-locals.el cleanup, added indentation for when-let

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,10 @@
 ((scheme-mode . ((eval
-				  put 'in-test-suite 'scheme-indent-function 'defun)
-				 (eval
-				  with-eval-after-load 'geiser-guile
-				  (let ((root-dir (file-name-directory
-								   (locate-dominating-file default-directory ".dir-locals.el"))))
-					(require 'cl-lib)
-					(cl-pushnew root-dir geiser-guile-load-path :test #'string-equal))))))
+                  put 'in-test-suite 'scheme-indent-function 'defun)
+                 (eval
+                  put 'when-let 'scheme-indent-function 2)
+                 (eval
+                  with-eval-after-load 'geiser-guile
+                  (let ((root-dir (file-name-directory
+                                   (locate-dominating-file default-directory ".dir-locals.el"))))
+                    (require 'cl-lib)
+                    (cl-pushnew root-dir geiser-guile-load-path :test #'string-equal))))))
