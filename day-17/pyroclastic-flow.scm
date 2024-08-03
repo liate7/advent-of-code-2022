95e2c815133f90e21c5c34e487057d42d72c9fa0
Andrew Patterson <andrewpatt7@gmail.com>
2024-08-03 14:19:03 -0400

    Final state of AoC 2022

--- day-17/pyroclastic-flow.scm
+++ day-17/pyroclastic-flow.scm
@@ -1,26 +1,28 @@
 (define-module (day-17 pyroclastic-flow)
   #:use-module (test)
   #:use-module (utils)
+  #:use-module (ice-9 arrays)
   #:use-module (ice-9 match)
+  #:use-module (ice-9 pretty-print)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-9)
   #:use-module (srfi srfi-9 gnu))
 
 (define rocks
-  (circular-list
-   "####"
-   (format #f ".#.~%~
-               ###~%~
-               .#.")
-   (format #f "..#~%~
-               ..#~%~
-               ###")
-   (format #f "#~%~
-               #~%~
-               #~%~
-               #")
-   (format #f "##~%~
-               ##")))
+  (map (curry list->array 2)
+       '(((#\# #\# #\# #\#))
+         ((_   #\# _ )
+          (#\# #\# #\#)
+          (_   #\# _ ))
+         ((_   _   #\#)
+          (_   _   #\#)
+          (#\# #\# #\#))
+         ((#\#)
+          (#\#)
+          (#\#)
+          (#\#))
+         ((#\# #\#)
+          (#\# #\#)))))
 
 (define test-input ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")
 
@@ -30,5 +32,42 @@
 
 
 
-(define (star-1 lines)
+(define-immutable-record-type <chamber>
+  (%make-chamber repr rocks jets)
+  chamber?
+  (repr  chamber-list  set-chamber-list)
+  (rocks chamber-rocks set-chamber-rocks)
+  (jets  chamber-jets  set-chamber-jets))
+
+(define char->direction 
+  (match-lambda
+    (#\> 'right)
+    (#\< 'left)))
+
+(define (parse-directions input)
+  (map char->direction (string->list input)))
+
+(define (make-chamber input)
+  (%make-chamber '()
+                 (apply circular-list rocks)
+                 (apply circular-list
+                        (parse-directions input))))
+
+(define-immutable-record-type <point>
+  (make-point x y)
+  point?
+  (x point-x set-point-x)
+  (y point-y set-point-y))
+
+(define (display-point point port)
+  (format port "#<point ~a ~a>"
+          (point-x point)
+          (point-y point)))
+
+(set-record-type-printer! <point> display-point)
+
+(define (piece-step chamber left-edge-position)
+  ())
+
+(define (star-1 input)
   (error "Not implemented yet"))
